de473ee0b66f91f9c5a1ae2618386df3b4228349
Carolina Oliveira <carolinasao6@gmail.com>
2021-07-09 12:41:48 -0300

    Resolvido os erros das 3 utlimas funcoes

--- kain.py
+++ kain.py
@@ -13,25 +13,25 @@ def standardize_title(title):
 
     return new_title
 
-text_1 = """a famosa atriz Constance Rattigan recebe uma encomenda desagradável: uma lista com números de telefone de pessoas que morreram recentemente. é uma coisa assustadora considerando que os nomes das poucas pessoas vivas presentes na lista estão assinalados em vermelho com uma cruz. O da própria Constance é um deles."""
+text_1 = """a famosa atriz Constance Rattigan recebe uma encomenda desagradável: uma lista com números de telefone de pessoas que morreram recentemente. é uma coisa assustadora, considerando que os nomes das poucas pessoas vivas presentes na lista estão assinalados em vermelho com uma cruz. O da própria Constance é um deles."""
 
 def standardize_text(text_1):
 
-    filtered = re.compile('([.?!]\s*)')
-    split_text = filtered.split(text_1)
+    text_1 = text_1.split('. ')
 
-    normal_text = ''.join([i.capitalize() for i in split_text])
+    for index, phrase in enumerate(text_1):
+        text_1[index] = phrase[0].upper() + phrase[1:]
 
-    final_text = normal_text.strip().capitalize()
-
-    return final_text
+    text_1 = ". ".join(text_1)
+    
+    return text_1
 
 text = "pense num deserto"
 
 def title_creator(text):
 
     new_text = text.title()
-    center_text = new_text.center(57, "-")
+    center_text = new_text.center(len(new_text) + 40, "-")
 
     return center_text
 
@@ -48,11 +48,22 @@ complexa e      bela ao mesmo tempo.
 
 def text_merge(text_of_blog_a, text_of_blog_b):
 
-    one_text = text_of_blog_a + text_of_blog_b
-    upper_text = standardize_text(one_text)
-    final_text = re.sub("\s+", " ", upper_text)
+    text_of_blog_a = text_of_blog_a.split()
+    text_of_blog_b = text_of_blog_b.split()
 
-    return final_text
+    text_of_blog_a = ' '.join(text_of_blog_a)
+    text_of_blog_b = ' '.join(text_of_blog_b)
+
+    text_of_blog_a = standardize_text(text_of_blog_a)
+    text_of_blog_b = standardize_text(text_of_blog_b)
+
+    text_of_blog_a = text_of_blog_a[:-1]
+
+    text_of_blog_b = text_of_blog_b[0].lower() + text_of_blog_b[1:]
+
+    one_text = text_of_blog_a + " " + text_of_blog_b
+
+    return one_text
     
 if __name__ == "__main__":
 
